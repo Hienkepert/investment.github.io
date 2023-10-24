1546027034ebee069bfae31450bf6776a41361b5
Macintosh HD <macintoshhd@ip-172-29-42-192.ap-southeast-1.compute.internal>
2023-10-24 16:52:16 +0700

    test7

--- investment.js
+++ investment.js
@@ -1,13 +1,28 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Display today's date
+    // Display today's date (left-aligned)
+    document.getElementById("currentDate").style.textAlign = "left";
     document.getElementById("currentDate").innerText = new Date().toLocaleDateString();
   
     // Add event listener for Calculate button
     document.getElementById("calculateBtn").addEventListener("click", calculateTotalInvestment);
   
+    // Function to format numbers with thousand separators
+    function formatNumberWithCommas(number) {
+      return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+  
     // Event listener for updating the calculated cost
-    document.getElementById("quycach").addEventListener("input", calculateCost);
-    document.getElementById("price").addEventListener("input", calculateCost);
+    document.getElementById("soluong").addEventListener("input", function() {
+      calculateCost();
+      // Format the value with thousand separators
+      this.value = formatNumberWithCommas(parseFloat(this.value) || 0);
+    });
+  
+    document.getElementById("price").addEventListener("input", function() {
+      calculateCost();
+      // Format the value with thousand separators
+      this.value = formatNumberWithCommas(parseFloat(this.value) || 0);
+    });
   });
   
   function calculateTotalInvestment() {
@@ -32,21 +47,24 @@ document.addEventListener("DOMContentLoaded", function() {
     // Perform calculations for total costs
     const totalCosts = totalFixedCosts + totalVariableCosts;
   
-    // Display results
-    document.getElementById("totalFixedCosts").innerText = `Total Fixed Costs: ${totalFixedCosts}`;
-    document.getElementById("totalVariableCosts").innerText = `Total Variable Costs: ${totalVariableCosts}`;
-    document.getElementById("totalCosts").innerText = `Total Costs: ${totalCosts}`;
+    // Display results with thousand separators
+    document.getElementById("totalFixedCosts").innerText = `Total Fixed Costs: ${formatNumberWithCommas(totalFixedCosts)}`;
+    document.getElementById("totalVariableCosts").innerText = `Total Variable Costs: ${formatNumberWithCommas(totalVariableCosts)}`;
+    document.getElementById("totalCosts").innerText = `Total Costs: ${formatNumberWithCommas(totalCosts)}`;
   }
   
-  // Function to calculate the cost based on quycach and price
+  // Function to calculate the cost based on soluong and price
   function calculateCost() {
-    const quycach = parseFloat(document.getElementById("quycach").value) || 0;
+    const soluong = parseFloat(document.getElementById("soluong").value) || 0;
     const price = parseFloat(document.getElementById("price").value) || 0;
   
     // Calculate the cost
-    const cost = quycach * price;
+    const cost = soluong * price;
   
-    // Display the calculated cost
-    document.getElementById("calculatedCost").textContent = cost.toFixed(2); // Display cost with 2 decimal places
+    // Round the cost to the nearest integer (remove decimal places)
+    const roundedCost = cost.toFixed(0);
+  
+    // Display the calculated cost without decimal places with thousand separators
+    document.getElementById("calculatedCost").textContent = formatNumberWithCommas(roundedCost);
   }
   
