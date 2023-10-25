7f24813864d9a59ebfe798c25112a55b3a35915d
Macintosh HD <macintoshhd@ip-172-29-42-192.ap-southeast-1.compute.internal>
2023-10-25 09:18:51 +0700

    test

--- investment.js
+++ investment.js
@@ -1,15 +1,32 @@
-    document.addEventListener("DOMContentLoaded", function() {
+// Function to format numbers with thousand separators
+function formatNumberWithCommas(number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+  
+  // Function to calculate the cost based on soluong and price
+  function calculateCost() {
+    const soluong = parseFloat(document.getElementById("soluong").value) || 0;
+    const price = parseFloat(document.getElementById("price").value) || 0;
+  
+    // Calculate the cost
+    const cost = soluong * price;
+  
+    // Round the cost to the nearest integer (remove decimal places)
+    const roundedCost = cost.toFixed(0);
+  
+    // Display the calculated cost without decimal places with thousand separators
+    document.getElementById("calculatedCost").textContent = formatNumberWithCommas(roundedCost);
+  
+    return roundedCost;  // Return the rounded cost for use in other functions
+  }
+  
+  document.addEventListener("DOMContentLoaded", function() {
     // Display today's date (left-aligned)
     document.getElementById("currentDate").style.textAlign = "left";
     document.getElementById("currentDate").innerText = new Date().toLocaleDateString();
   
     // Add event listener for Calculate button
     document.getElementById("calculateBtn").addEventListener("click", calculateTotalInvestment);
-  
-    // Function to format numbers with thousand separators
-    function formatNumberWithCommas(number) {
-      return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    }
   
     // Event listener for updating the calculated cost
     document.getElementById("soluong").addEventListener("input", function() {
@@ -38,32 +55,21 @@
     // Sum up land preparation costs
     const totalLandPreparation = landPrep1 + landPrep2 + landPrep3 + landPrep4;
   
+    // Get the rounded cost from the calculateCost function
+    const roundedCost = calculateCost();
+  
+    // Get values for variable costs
+    const totalVariableCosts = totalLandPreparation + parseFloat(roundedCost);
+  
     // Sum up all fixed costs
     const totalFixedCosts = landRent;
   
     // Perform calculations for total costs
     const totalCosts = totalFixedCosts + totalVariableCosts;
-
-      // Function to calculate the cost based on soluong and price
-  function calculateCost() {
-    const soluong = parseFloat(document.getElementById("soluong").value) || 0;
-    const price = parseFloat(document.getElementById("price").value) || 0;
-  
-    // Calculate the cost
-    const cost = soluong * price;
-  
-    // Round the cost to the nearest integer (remove decimal places)
-    const roundedCost = cost.toFixed(0);
-  
-    // Display the calculated cost without decimal places with thousand separators
-    document.getElementById("calculatedCost").textContent = formatNumberWithCommas(roundedCost);
-  }
-  
-    // Get values for variable costs (placeholder)
-    const totalVariableCosts = totalLandPreparation + RoundedCost;
   
     // Display results with thousand separators
     document.getElementById("totalFixedCosts").innerText = `Total Fixed Costs: ${formatNumberWithCommas(totalFixedCosts)}`;
     document.getElementById("totalVariableCosts").innerText = `Total Variable Costs: ${formatNumberWithCommas(totalVariableCosts)}`;
     document.getElementById("totalCosts").innerText = `Total Costs: ${formatNumberWithCommas(totalCosts)}`;
   }
+  
